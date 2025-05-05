fba0645544ac74d574877ec67d72dbffdee960ef
BlvdBroken <skazimirowicz@gmail.com>
2025-05-05 02:04:09 -0700

    Codename SQL
    Added multiple server support, as well as memory between resets. More testing might be required but I just kinda want to go to bed. Known bug happens when DMing "test" to the bot, will fix later.

--- klxn.py
+++ klxn.py
@@ -2,8 +2,9 @@ import discord
 import json
 import time
 import cv2
-import re
+import string
 import numpy as np
+import sqlite3
 from PIL import Image, ImageDraw, ImageFont
 from moviepy.editor import ImageClip, concatenate_videoclips, AudioFileClip
 
@@ -14,32 +15,67 @@ with open('config.json') as f:
 # on message, parses
 class MyClient(discord.Client):
 
-    word = "test"
-    rgx = rf"\b{word}\b"
-    usr = None
+    words = {} # dict of serverid : (word, timeof, user) with timeof being time since said and not reset and user being id of last user said
     ignored = [] # ignored channels
     optedout = [] # opted out users
     timeof = 0  # time since said and not reset
+    
+    # sql presets
+    create_table = """CREATE TABLE IF NOT EXISTS words (
+        server_id INTEGER PRIMARY KEY,
+        word TEXT DEFAULT 'test',
+        timeof INTEGER DEFAULT 0 NOT NULL,
+        user INTEGER DEFAULT NULL
+    );"""
+    insert_table = """INSERT INTO words(server_id, word, timeof, user)
+        VALUES(?,?,?,?)
+    """
+    update_table = 'UPDATE words SET word = ?, timeof = ?, user = ? WHERE server_id = ?'
+    find_table = 'SELECT * FROM words WHERE server_id = ?'
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
+        # creates a SQLite 3 database if you don't have one
+        try:
+            with sqlite3.connect("serverwords.db") as conn:
+                print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+                cursor = conn.cursor()
+                cursor.execute(self.create_table)
+                conn.commit()
+        except sqlite3.OperationalError as e:
+            print("Failed to open database:", e)
 
     async def on_message(self, message):
         
+        # id of the server it is said in
+        serverid = message.guild.id if (type(message.channel) is not discord.channel.DMChannel) else None
+        
+        # if not currently in dictionary of servers (like on bot reset), check if in sql, add if so, make new entry if not
+        if serverid not in self.words:
+            with sqlite3.connect("serverwords.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute(self.find_table, (serverid,))
+                exists = cursor.fetchone()
+                if exists:
+                    self.words[serverid] = exists[1:]
+                else:
+                    cursor.execute(self.insert_table, (serverid, "test", 0, None))
+                    self.words[serverid] = ("test", 0, None)
+                    
+        print(self.words[serverid])
+                    
         # if been 24h since said and not reset
-        if (self.timeof > 0) and (time.time() >= self.timeof + 86400):
-            self.word = "klaxon"
-            self.rgx = rf"\bklaxon\b"
-            self.generate_klaxon_mp4(self.word.upper()) # generate video
-            self.timeof = 0
-            await self.usr.send("I haven't heard from you in 24 hours, resetting the word to \"klaxon\"")
+        if (self.words[serverid][1] > 0) and (time.time() >= self.words[serverid][1] + 86400):
+            await self.words[serverid][2].send("I haven't heard from you in 24 hours, resetting the word to \"klaxon\"")
+            self.words[serverid] = ("klaxon", 0, None)
+            self.generate_klaxon_mp4(self.words[serverid][0].upper()) # generate video
             return
 
         # does nothing if bots say it
-        if message.author == self.user:
+        if message.author.bot:
             return
-        
+                
         # ignore channels
         if message.content.startswith("k!ignore"):
             if message.channel.id in self.ignored:
@@ -69,38 +105,46 @@ class MyClient(discord.Client):
             return
 
         # klaxon word finder
-        if self.word and (re.search(self.rgx, message.content.lower()):
-            self.usr = message.author
-            
-            if self.word == "test":
-                await message.channel.send("# :camera_with_flash: The Klaxon word \'{0}\' was said by <@{1}> and earned them -10 points! :camera_with_flash:".format(self.word, message.author.id), file=discord.File("klaxon_test.mp4", filename="klaxon.mp4"))
+        if self.words[serverid][0] and (self.check_in_message(message.content, self.words[serverid][0])):
+            self.generate_klaxon_mp4(self.words[serverid][0].upper()) # generate video          
+            if self.words[serverid][0] == "test":
+                await message.channel.send("# :camera_with_flash: The Klaxon word \'{0}\' was said by <@{1}> and earned them -10 points! :camera_with_flash:".format(self.words[serverid][0], message.author.id), file=discord.File("klaxon_test.mp4", filename="klaxon.mp4"))
             else:
-                await message.channel.send("# :camera_with_flash: The Klaxon word \'{0}\' was said by <@{1}> and earned them -10 points! :camera_with_flash:".format(self.word, message.author.id), file=discord.File("klaxon.mp4", filename="klaxon.mp4"))
-
-            self.word = None
-            self.rgx = None
+                await message.channel.send("# :camera_with_flash: The Klaxon word \'{0}\' was said by <@{1}> and earned them -10 points! :camera_with_flash:".format(self.words[serverid][0], message.author.id), file=discord.File("klaxon.mp4", filename="klaxon.mp4"))
+            self.words[serverid] = (None, time.time(), message.author.id)
+            with sqlite3.connect("serverwords.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute(self.update_table, (None, time.time(), message.author.id, serverid))
             await message.author.send("Please respond with a new Klaxon word. Choose wisely.")
-            self.timeof = time.time()
             print("{0} said by {1} in {2}".format(message.content, message.author.name, message.channel.name))
             return
     
         # user dm understander
-        if (type(message.channel) is discord.channel.DMChannel) and self.usr and (self.usr == message.author):
-            tempword = message.content.lower().strip()
-            if tempword.isalpha():
-                self.word = tempword
-                self.rgx = rf"\b{tempword}\b"
-                await message.author.send("Setting word...")
-                self.timeof = 0
-                self.generate_klaxon_mp4(self.word.upper()) # generate video
-                await message.author.send("You have selected \'{0}\' as the new Klaxon word.".format(self.word))
-            else:
-                await message.author.send("I said word. \'{0}\' isn't a word. Idiot.".format(tempword))
+        if (type(message.channel) is discord.channel.DMChannel):
+            userlist = list(tpl[2] for tpl in self.words.values())
+            if (message.author.id in userlist):
+                tempword = message.content.lower().strip()
+                if tempword.isalpha():
+                    if (userlist.count(message.author.id) > 1):
+                        await message.author.send("You've managed to trigger the Klaxon in multiple servers. For this unprecedented level of stupidity, your word will apply to all relevant servers.")
+                    for servid, wordinfo in self.words.items():
+                        if (message.author.id == wordinfo[2]):
+                            self.words[servid] = (tempword, 0, None)
+                            with sqlite3.connect("serverwords.db") as conn:
+                                cursor = conn.cursor()
+                                cursor.execute(self.update_table, (tempword, 0, None, servid))
+                    await message.author.send("You have selected \'{0}\' as the new Klaxon word.".format(tempword))
+                else:
+                    await message.author.send("I said word. \'{0}\' isn't a word. Idiot.".format(tempword))
             return
-        
+    
+    # checks if word is in the message, not as a substring
+    @staticmethod
+    def check_in_message(mess, word):
+        translator = str.maketrans(string.punctuation, ' '*len(string.punctuation)) # map punctuation to space (based)
+        return word in mess.translate(translator).split()
 
     # make mp4 file
-
     def generate_klaxon_mp4(self, klaxon_word):
         def text2png(text):
             width, height = 1418, 1072
