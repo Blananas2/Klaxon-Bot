709a39e8202aa1c912db68707fa21888b67870a8
BlvdBroken <skazimirowicz@gmail.com>
2025-10-02 19:38:49 -0700

    v1.1 Update
    Word now resets after not being triggered for an amount of time (default 30d)
    Commands like ignore and optout actually save between updates
    Added new commands for help, prefix and reset time
    Bot now works with non-latin characters (thanks Quinn)
    Updated README
    Added catch lines for old hosts of the bot to update their db without error/deleting the old dbs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Klaxon-Bot
+## v1.1
 Sounds an alarm when someone says the chosen word.
 
-Use tinyurl.com/klaxonbot (https://discord.com/oauth2/authorize?client_id=1281154203091337268&permissions=274878417984&integration_type=0&scope=bot+applications.commands) to invite the bot to your server. The default word is 'klaxon'.
+Use [tinyurl.com/klaxonbot](https://discord.com/oauth2/authorize?client_id=1281154203091337268&permissions=274878417984&integration_type=0&scope=bot+applications.commands) to invite the bot to your server. The default word is 'klaxon'. Use k!help for commands.
 
-To test for PRs or personal use, go to the Discord Developer Portal (https://discord.com/developers/applications) and create a new application. Create a config.json and add the token from this application to it. Then, run klxn.py after installing the dependancies at the top.
+To test for PRs or personal use, go to the [Discord Developer Portal](https://discord.com/developers/applications) and create a new application with SERVER MEMBERS INTENT and MESSAGE CONTENT INTENT enabled in the Bot settings. Create a config.json and add the token from this application to it. Then, run klxn.py after installing the dependancies at the top (install moviepy V1.0.3 using pip install moviepy==1.0.3 if you're having trouble with moviepy.editor).
--- klxn.py
+++ klxn.py
@@ -15,33 +15,47 @@ with open('config.json') as f:
 # on message, parses
 class MyClient(discord.Client):
 
-    words = {} # dict of serverid : (word, timeof, user) with timeof being time since said and not reset and user being id of last user said
-    ignored = [] # ignored channels
-    optedout = [] # opted out users
-    timeof = 0  # time since said and not reset
+    words = {}  # dict of serverid : (word, timeofr, user, timeofs) (klaxon word, time since said and not reset, user waiting on reset, time since set)
+    serverinfo = {}  # dict of serverid : [prefix, ignored, optedout, resetd] [bot command prefix, ingored channels, opted out users, days before reset]
     
     # sql presets
-    create_table = """CREATE TABLE IF NOT EXISTS words (
+    create_words_table = """CREATE TABLE IF NOT EXISTS words (
         server_id INTEGER PRIMARY KEY,
         word TEXT DEFAULT 'klaxon',
-        timeof INTEGER DEFAULT 0 NOT NULL,
-        user INTEGER DEFAULT NULL
+        timeofr INTEGER DEFAULT 0 NOT NULL,
+        user INTEGER DEFAULT NULL,
+        timeofs INTEGER DEFAULT 0 NOT NULL
     );"""
-    insert_table = """INSERT INTO words(server_id, word, timeof, user)
-        VALUES(?,?,?,?)
+    insert_words_table = """INSERT INTO words(server_id, word, timeofr, user, timeofs)
+        VALUES(?,?,?,?,?)
     """
-    update_table = 'UPDATE words SET word = ?, timeof = ?, user = ? WHERE server_id = ?'
-    find_table = 'SELECT * FROM words WHERE server_id = ?'
+    update_words_table = 'UPDATE words SET word = ?, timeofr = ?, user = ?, timeofs = ? WHERE server_id = ?'
+    find_words_table = 'SELECT * FROM words WHERE server_id = ?'
+    
+    create_serverinfo_table = """CREATE TABLE IF NOT EXISTS serverinfo (
+        server_id INTEGER PRIMARY KEY,
+        prefix TEXT DEFAULT 'k!' NOT NULL,
+        ignored TEXT DEFAULT NULL,
+        optedout TEXT DEFAULT NULL,
+        resetd INTEGER DEFAULT 30
+    );"""
+    insert_serverinfo_table = """INSERT INTO serverinfo(server_id, prefix, ignored, optedout, resetd)
+        VALUES(?,?,?,?,?)
+    """
+    # update_serverinfo_table = 'UPDATE serverinfo SET prefix = ?, ignored = ?, optedout = ? WHERE server_id = ?'
+    find_serverinfo_table = 'SELECT * FROM serverinfo WHERE server_id = ?'
+    
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print("Klaxon Bot v1.0")
+        print("Klaxon Bot v1.1")
         # creates a SQLite 3 database if you don't have one
         try:
             with sqlite3.connect("serverwords.db") as conn:
                 print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
                 cursor = conn.cursor()
-                cursor.execute(self.create_table)
+                cursor.execute(self.create_words_table)
+                cursor.execute(self.create_serverinfo_table)
                 conn.commit()
         except sqlite3.OperationalError as e:
             print("Failed to open database:", e)
@@ -50,69 +64,164 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         
-        # id of the server it is said in
-        serverid = message.guild.id if (type(message.channel) is not discord.channel.DMChannel) else None
-        
-        # if not currently in dictionary of servers (like on bot reset), check if in sql, add if so, make new entry if not
-        if serverid and serverid not in self.words:
-            with sqlite3.connect("serverwords.db") as conn:
-                cursor = conn.cursor()
-                cursor.execute(self.find_table, (serverid,))
-                exists = cursor.fetchone()
-                if exists:
-                    self.words[serverid] = exists[1:]
-                else:
-                    cursor.execute(self.insert_table, (serverid, "klaxon", 0, None))
-                    self.words[serverid] = ("klaxon", 0, None)
-                    
-        print((self.words[serverid]) if serverid else ("DM from " + message.author.display_name))
-                    
-        # if been 24h since said and not reset
-        if serverid and (self.words[serverid][1] > 0) and (time.time() >= self.words[serverid][1] + 86400): # 86400 is 24h
-            await self.get_user(self.words[serverid][2]).send("I haven't heard from you in 24 hours, resetting the word to \"klaxon\"")
-            self.words[serverid] = ("klaxon", 0, None)
-            self.generate_klaxon_mp4(self.words[serverid][0].upper()) # generate video
-            return
-
         # does nothing if bots say it
         if message.author.bot:
             return
-                
-        # ignore channels
-        if message.content.startswith("k!ignore"):
-            if message.channel.id in self.ignored:
-                self.ignored.remove(message.channel.id)
-                await message.channel.send("<#{0}> unignored.".format(message.channel.id))
-            else:
-                self.ignored.append(message.channel.id)
-                await message.channel.send("<#{0}> ignored.".format(message.channel.id))
+        
+        # id of the server it is said in
+        serverid = message.guild.id if (type(message.channel) is not discord.channel.DMChannel) else None
+        
+        # user dm understander
+        if (type(message.channel) is discord.channel.DMChannel):
+            userlist = list(tpl[2] for tpl in self.words.values())  # gets a list of all stored users who need to reset
+            if (message.author.id in userlist):
+                tempword = message.content.lower().strip()
+                if tempword.isalpha():
+                    if (userlist.count(message.author.id) > 1):
+                        await message.author.send("You've managed to trigger the Klaxon in multiple servers. For this unprecedented level of stupidity, your word will apply to all relevant servers.")
+                    for servid, wordinfo in self.words.items():  # for loop through all of the set words allows for someone to set multiple if triggered by same person
+                        if (message.author.id == wordinfo[2]):
+                            self.words[servid] = (tempword, 0, None, time.time())
+                            with sqlite3.connect("serverwords.db") as conn:
+                                cursor = conn.cursor()
+                                cursor.execute(self.update_words_table, (tempword, 0, None, time.time(), servid))
+                    await message.author.send("You have selected \'{0}\' as the new Klaxon word.".format(tempword))
+                else:  # if it has non-alpha characters like numbers or spaces
+                    await message.author.send("I said word. \'{0}\' isn't a word. Idiot.".format(tempword))
+            print("DM from " + str(message.author.display_name))
             return
         
-        # allow opt-out of users
-        if message.content.startswith("k!optout"):
-            if message.author in self.optedout:
-                self.optedout.remove(message.author)
-                await message.channel.send("You have opted-in to being klaxoned.")
-            else:
-                self.optedout.append(message.author)
-                await message.channel.send("You have opted-out of being klaxoned.")
-            return
+        # if not currently in dictionary of servers (like on bot reset), check if in sql, add if so, make new entry if not
+        if serverid not in self.words:
+            with sqlite3.connect("serverwords.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute(self.find_words_table, (serverid,))
+                exists = cursor.fetchone()
+                if exists:
+                    if len(exists) != 5:  # hard coded in for every breaking update that changes db entry formnat to make previous db entries backwards compatible
+                        cursor.execute('ALTER TABLE words ADD COLUMN timeofs INTEGER DEFAULT 0 NOT NULL')
+                        cursor.execute('UPDATE words SET timeofs = ? WHERE server_id = ?', (time.time(), serverid))
+                        cursor.execute(self.find_words_table, (serverid,))
+                        exists = cursor.fetchone()
+                    self.words[serverid] = exists[1:]
+                else:
+                    cursor.execute(self.insert_words_table, (serverid, "klaxon", 0, None, time.time()))
+                    self.words[serverid] = ("klaxon", 0, None, time.time())
+
+        # same as above but with serverinfo
+        if serverid not in self.serverinfo:
+            with sqlite3.connect("serverwords.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute(self.find_serverinfo_table, (serverid,))
+                exists = cursor.fetchone()
+                if exists:
+                    if len(exists) != 5:  # hard coded in for every breaking update that changes db entry formnat to make previous db entries backwards compatible
+                        cursor.execute('ALTER TABLE serverinfo ADD COLUMN resetd INTEGER DEFAULT 30 NOT NULL')
+                        cursor.execute(self.find_words_table, (serverid,))
+                        exists = cursor.fetchone()
+                    self.serverinfo[serverid] = list(exists[1:])
+                else:
+                    cursor.execute(self.insert_serverinfo_table, (serverid, "k!", None, None, 30))
+                    self.serverinfo[serverid] = ["k!", "", "", 30]
+                    
+        print(self.words[serverid])  # testing
+                    
+        # if been 24h since said and not reset
+        if (self.words[serverid][1] > 0) and (time.time() >= self.words[serverid][1] + 86400):  # 86400 is 24h
+            await self.get_user(self.words[serverid][2]).send("I haven't heard from you in 24 hours, resetting the word to \"klaxon\"")
+            self.words[serverid] = ("klaxon", 0, None, time.time())
+            with sqlite3.connect("serverwords.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute(self.update_words_table, ("klaxon", 0, None, time.time(), serverid))
+            # self.generate_klaxon_mp4(self.words[serverid][0].upper()) # generate video
+        
+        # if been 30d since said and not triggered
+        if (self.words[serverid][3] > 0) and (time.time() >= self.words[serverid][3] + (86400 * self.serverinfo[serverid][3])):  # 86400 is 1d
+            self.words[serverid] = ("klaxon", 0, None, time.time())
+            with sqlite3.connect("serverwords.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute(self.update_words_table, ("klaxon", 0, None, time.time(), serverid))
+        
+        # if command (starts with prefix)
+        if message.content.startswith(self.serverinfo[serverid][0]):
+            command = message.content.removeprefix(self.serverinfo[serverid][0])  # remove prefix to check command
+            
+            if command.startswith("help"):
+                await message.channel.send( \
+                    "**{0}prefix `s`** - changes the current prefix for commands to `s` (default is k!).\n" \
+                    "**{0}ignore** - Toggles whether future messages in the current channel will trigger the Klaxon\n" \
+                    "**{0}optout** - Toggles whether the command's author will trigger the Klaxon\n" \
+                    "**{0}timer** - Sets how long, in days, before an untriggered word is reset to 'klaxon' (default 30 days)".format(self.serverinfo[serverid][0]))
+            
+            if command.startswith("prefix "):  # updates prefix
+                newpre = command.removeprefix("prefix ")
+                self.serverinfo[serverid][0] = newpre
+                with sqlite3.connect("serverwords.db") as conn:  # update db after ignore/unignore
+                        cursor = conn.cursor()
+                        cursor.execute('UPDATE serverinfo SET prefix = ? WHERE server_id = ?', (newpre, serverid))
+                await message.channel.send("The new prefix for this server is `{0}`".format(newpre))
+            
+            if command.startswith("ignore"):  # ignores channels
+                ign = self.serverinfo[serverid][1]
+                if not ign:  # if list is empty
+                    self.serverinfo[serverid][1] = str(message.channel.id)
+                    await message.channel.send("<#{0}> ignored.".format(message.channel.id))
+                elif str(message.channel.id) in ign:
+                    self.serverinfo[serverid][1] = ign.replace(str(message.channel.id), "").replace(",,", ",").strip(",")  # removes from string then clears errant commas
+                    await message.channel.send("<#{0}> unignored.".format(message.channel.id))
+                else:
+                    self.serverinfo[serverid][1] += "," + str(message.channel.id)
+                    await message.channel.send("<#{0}> ignored.".format(message.channel.id))
+                with sqlite3.connect("serverwords.db") as conn:  # update db after ignore/unignore
+                        cursor = conn.cursor()
+                        cursor.execute('UPDATE serverinfo SET ignored = ? WHERE server_id = ?', (self.serverinfo[serverid][1], serverid))
+              
+            if command.startswith("optout"):  # opts out users
+                opt = self.serverinfo[serverid][2]
+                if not opt:  # if list is empty
+                    self.serverinfo[serverid][2] = str(message.author.id)
+                    await message.channel.send("You have opted-out of being klaxoned.")
+                elif str(message.author.id) in opt:
+                    self.serverinfo[serverid][2] = opt.replace(str(message.author.id), "").replace(",,", ",").strip(",")  # removes from string then clears errant commas
+                    await message.channel.send("You have opted-in to being klaxoned.")
+                else:
+                    self.serverinfo[serverid][2] += "," + str(message.author.id)
+                    await message.channel.send("You have opted-out of being klaxoned.")
+                with sqlite3.connect("serverwords.db") as conn:  # update db after ignore/unignore
+                        cursor = conn.cursor()
+                        cursor.execute('UPDATE serverinfo SET optedout = ? WHERE server_id = ?', (self.serverinfo[serverid][2], serverid))
+                        
+            if command.startswith("timer "):  # sets time in days before reset
+                days = command.removeprefix("timer ")
+                if not days.isnumeric():
+                    await message.channel.send("Hey buddy, did you just blow in from stupid town? That's not a number.")
+                elif days == "0":
+                    await message.channel.send("I bet you think you're real funny.")
+                else:
+                    self.serverinfo[serverid][3] = int(days)
+                    await message.channel.send("The Klaxon will now reset after {0} days.".format(days))
+                    with sqlite3.connect("serverwords.db") as conn:  # update db after ignore/unignore
+                        cursor = conn.cursor()
+                        cursor.execute('UPDATE serverinfo SET resetd = ? WHERE server_id = ?', (self.serverinfo[serverid][3], serverid))
+                    
+                        
+            print(self.serverinfo[serverid])
         
         # do not listen to ignored channels (except ignore command)
-        if message.channel.id in self.ignored:
+        if self.serverinfo[serverid][1] and (str(message.channel.id) in self.serverinfo[serverid][1]):
             return
         
         # ignore opted-out users
-        if message.author in self.optedout:
+        if self.serverinfo[serverid][2] and (str(message.author.id) in self.serverinfo[serverid][2]):
             return
 
         # klaxon word finder
-        if serverid and self.words[serverid][0] and (self.check_in_message(message.content, self.words[serverid][0])):
+        if self.words[serverid][0] and (self.check_in_message(message.content, self.words[serverid][0])):
             wordsaid = self.words[serverid][0]
-            self.words[serverid] = (None, time.time(), message.author.id)
+            self.words[serverid] = (None, time.time(), message.author.id, 0)
             with sqlite3.connect("serverwords.db") as conn:
                 cursor = conn.cursor()
-                cursor.execute(self.update_table, (None, time.time(), message.author.id, serverid))
+                cursor.execute(self.update_words_table, (None, time.time(), message.author.id, 0, serverid))
             await message.channel.typing() # posts klaxon bot is typing... while generating video
             self.generate_klaxon_mp4(wordsaid.upper()) # generate video          
             if self.words[serverid][0] == "test":
@@ -123,29 +232,10 @@ class MyClient(discord.Client):
             print("{0} said by {1} in {2}".format(message.content, message.author.name, message.channel.name))
             return
     
-        # user dm understander
-        if (type(message.channel) is discord.channel.DMChannel):
-            userlist = list(tpl[2] for tpl in self.words.values())
-            if (message.author.id in userlist):
-                tempword = message.content.lower().strip()
-                if tempword.isalpha():
-                    if (userlist.count(message.author.id) > 1):
-                        await message.author.send("You've managed to trigger the Klaxon in multiple servers. For this unprecedented level of stupidity, your word will apply to all relevant servers.")
-                    for servid, wordinfo in self.words.items():
-                        if (message.author.id == wordinfo[2]):
-                            self.words[servid] = (tempword, 0, None)
-                            with sqlite3.connect("serverwords.db") as conn:
-                                cursor = conn.cursor()
-                                cursor.execute(self.update_table, (tempword, 0, None, servid))
-                    await message.author.send("You have selected \'{0}\' as the new Klaxon word.".format(tempword))
-                else:
-                    await message.author.send("I said word. \'{0}\' isn't a word. Idiot.".format(tempword))
-            return
-    
     # checks if word is in the message, not as a substring
     @staticmethod
     def check_in_message(mess, word):
-        translator = str.maketrans(string.punctuation, ' '*len(string.punctuation)) # map punctuation to space (based)
+        translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))  # map punctuation to space (based)
         return word in mess.lower().translate(translator).split()
 
     # make mp4 file
